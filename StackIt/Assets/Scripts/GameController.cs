80116c452628fea33d3368c6c61f3c8573f8344d
Maynor Gustavo <uk004992@student.uni-kassel.de>
2020-12-18 00:03:12 +0100

    Counter Added  & need be Fixed [not really works]

---
+++ StackIt/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public static int counter;
+    public TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        counter = 0;
+        SetCountText();
+    }
+    void Update()
+    {
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            //bring down the Quader
+            QuaderController.quaderFallen = true;
+        }
+        SetCountText();
+        // Debug.Log("Counter " + counter);
+    }
+
+    public void SetCountText()
+    {
+        scoreText.text = "Score: " + counter.ToString();
+    }
+}
---
+++ StackIt/Assets/Scripts/QuaderController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuaderController : MonoBehaviour
+{
+
+    Vector3 pointA;
+    Vector3 pointB;
+
+    private float speed = 0.5f;
+    public static bool speedStoped;
+    public static bool quaderFallen;
+
+    void Start()
+    {
+        createQuader();
+        speedStoped = true;
+        quaderFallen = false;
+    }
+
+    void createQuader()
+    {
+        pointA = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        pointB = new Vector3(this.transform.position.x, this.transform.position.y, 3f);
+    }
+
+    void Update()
+    {
+        //Moved back and Fort
+        if (speedStoped)
+        {
+            float time = Mathf.PingPong(Time.time * speed, 1);
+            transform.position = Vector3.Lerp(pointA, pointB, time);
+        }
+        //this.GetComponent<GameController>().SetCountText();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Collition with the Sockel
+        if (other.tag == "Sockel")
+        {
+            SpawnManager.createQuader = false;
+            Destroy(this);
+            GameController.counter++;
+            Debug.Log("counter " + GameController.counter);
+           // GetComponent<GameController>().SetCountText();
+
+        }
+
+    }
+    void FixedUpdate()
+    {
+        if (quaderFallen)
+        {
+            this.GetComponent<Rigidbody>().useGravity = true;
+            this.GetComponent<Rigidbody>().isKinematic = false;
+            speedStoped = false;
+
+        }
+    }
+
+}
